5ad55ebcdc04cdb2dab3d169320483105a0d8d31
rougeyanWindows <se7ne_886@163.com>
2020-06-13 20:02:32 +0800

    commit Main标签

---
+++ src/Main.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="main">
+      <keep-alive :include=includeRoutes :exclude="excludeRoutes">
+        <router-view />
+      </keep-alive>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+  data() {
+      return {
+        includeRoutes: ["home"], // 被缓存的路由
+        excludeRoutes: [] // 不包含的路由
+      }
+  },
+  computed: {
+  },
+  created() {
+    // 通过eventbus来缓存路由
+  },
+  mounted() {
+  },
+  watch: {
+  },
+  methods: {
+  },
+  components: {
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
